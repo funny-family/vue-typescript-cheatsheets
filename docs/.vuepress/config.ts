e4d78d15edd728275dd19e26181088521014ccbe
funny-family <maksvadik228@gmail.com>
2022-03-12 15:52:58 +0700

    init

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,11 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  lang: 'en-US',
+  title: 'Vue TypeScript Cheatsheets',
+  description: 'This is my first VuePress site',
+
+  themeConfig: {
+    logo: 'https://vuejs.org/images/logo.png',
+  },
+});
---
+++ docs/README.md
@@ -0,0 +1,11 @@
+---
+home: true
+title: Home
+heroImage: /images/hero.png
+actions:
+  - text: Get Started
+    link: /guide/getting-started.html
+    type: primary
+
+footer: MIT Licensed | Copyright © {{ new Date().getFullYear() }} Vue TypeScript Cheatsheets
+---
