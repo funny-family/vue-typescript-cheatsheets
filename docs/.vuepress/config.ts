46f3071711f443d9db90856608a30548c839a802
funny family <maksvadik228@gmail.com>
2022-03-14 00:18:47 +0700

    update config file

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,11 +1,29 @@
-import { defineUserConfig } from 'vuepress'
+import { defineUserConfig } from 'vuepress';
 
 export default defineUserConfig({
   lang: 'en-US',
   title: 'Vue TypeScript Cheatsheets',
   description: 'This is my first VuePress site',
 
+  locales: {
+    '/': {
+      lang: 'en-US',
+      title: 'Vue TypeScript Cheatsheets',
+      description:
+        'Cheatsheets for Vue developers using TypeScript',
+    },
+  },
+
   themeConfig: {
-    logo: 'https://vuejs.org/images/logo.png',
+    logo: '/images/vue-plus-typescript.image.png',
+
+    docsDir: 'docs',
+
+    navbar: [
+      {
+        text: 'GitHub',
+        link: 'https://github.com/funny-family/vue-typescript-cheatsheet',
+      },
+    ],
   },
 });
