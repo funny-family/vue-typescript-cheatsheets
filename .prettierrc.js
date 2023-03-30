1e751131e95ae0c44b4c2d8c6e280208c94b4066
funny-family <maksvadik228@gmail.com>
2023-03-30 22:11:23 +0700

    add prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,12 @@
+/**
+ * @typedef { import("prettier").RequiredOptions } RequiredOption
+ */
+
+/** @type {RequiredOption} */
 module.exports = {
   singleQuote: true,
   trailingComma: 'all',
   printWidth: 80,
+  printWidth: 80,
+  endOfLine: 'lf',
 };
